cbe823ac1409d84dc5fd037a4d57e67ff700d8cf
Ryan Atkinson <mail@ryanatkn.com>
2023-09-22 09:19:50 -0600

    wip

--- src/lib/fuz_code_vite_plugins.ts
+++ src/lib/fuz_code_vite_plugins.ts
@@ -1,6 +1,8 @@
 import type {PluginOption} from 'vite';
 
 export const fuz_code_vite_plugins: PluginOption[] = [
+	// Ignore the default prismjs theme:
+	// TODO try to avoid flashing the original Prism theme during dev
 	{
 		name: 'remove_prism_css',
 		enforce: 'pre',
