ff0dcede628836aeea7a69bfece7a4a3e754c0db
Ryan Atkinson <mail@ryanatkn.com>
2023-10-16 16:17:36 -0600

    upgrade deps (#2)

---
+++ .changeset/new-points-tap.md
@@ -0,0 +1,5 @@
+---
+'@fuz.dev/fuz_code': patch
+---
+
+publish $lib/package.ts
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
 	import LibraryHeader from '@fuz.dev/fuz_library/LibraryHeader.svelte';
 	import LibraryFooter from '@fuz.dev/fuz_library/LibraryFooter.svelte';
-	import {parse_package_meta} from '@fuz.dev/fuz_library/package.js';
+	import {parse_package_meta} from '@fuz.dev/fuz_library/package_meta.js';
 
 	import CodeTome from '$routes/CodeTome.svelte';
+	import {package_json} from '$lib/package.js';
 
-	// TODO SvelteKit warns about this but we put `/static` in `/src` because of what it's saying,
-	/// maybe change to import as the first item from `packages`
-	import package_json from '../static/.well-known/package.json';
 	const pkg = parse_package_meta(package_json.homepage, package_json);
 </script>
 
