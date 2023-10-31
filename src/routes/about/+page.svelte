2f799bd93f6eeaf25bdd0a4bf6567a63d01978e1
Ryan Atkinson <mail@ryanatkn.com>
2023-10-31 11:43:26 -0600

    fix import

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import PackageDetail from '@fuz.dev/fuz_library/PackageDetail.svelte';
+	import Package_Detail from '@fuz.dev/fuz_library/Package_Detail.svelte';
 	import {parse_package_meta} from '@fuz.dev/fuz_library/package_meta.js';
 	import Library_Footer from '@fuz.dev/fuz_library/Library_Footer.svelte';
 	import Breadcrumb from '@fuz.dev/fuz_library/Breadcrumb.svelte';
@@ -22,7 +22,7 @@
 	</div>
 	<section class="box width_full spaced">
 		<div class="panel padded_md width_md">
-			<PackageDetail {pkg} />
+			<Package_Detail {pkg} />
 		</div>
 	</section>
 	<section class="box">
