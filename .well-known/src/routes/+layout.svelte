3bcbc15b34373669b93e1d412cb441aef766407e
Ryan Atkinson <mail@ryanatkn.com>
2024-02-15 22:35:51 -0700

    deployment

--- .well-known/src/routes/+layout.svelte
+++ .well-known/src/routes/+layout.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-	import '@fuz.dev/fuz/style.css';
-	import '@fuz.dev/fuz/theme.css';
+	import '@ryanatkn/fuz/style.css';
+	import '@ryanatkn/fuz/theme.css';
 	import '$lib/prism.css';
 
-	import Themed from '@fuz.dev/fuz/Themed.svelte';
+	import Themed from '@ryanatkn/fuz/Themed.svelte';
 </script>
 
 <svelte:head>
-	<title>@fuz.dev/fuz_code</title>
+	<title>@ryanatkn/fuz_code</title>
 </svelte:head>
 
 <Themed>
