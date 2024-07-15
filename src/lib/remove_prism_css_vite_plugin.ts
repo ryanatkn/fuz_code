73082df4676b76bae059259648e856aae5b40189
Ryan Atkinson <mail@ryanatkn.com>
2024-07-15 15:18:15 -0600

    upgrade `@ryanatkn/fuz@0.110.4` from `0.108.3`

--- src/lib/remove_prism_css_vite_plugin.ts
+++ src/lib/remove_prism_css_vite_plugin.ts
@@ -3,7 +3,7 @@ import type {PluginOption} from 'vite';
 /**
  * Ignores the default prismjs theme.
  */
-export const remove_prism_css = (matcher = /node_modules\/prismjs\/.+\.css$/u): PluginOption => ({
+export const remove_prism_css = (matcher = /node_modules\/prismjs\/.+\.css$/): PluginOption => ({
 	name: 'remove_prism_css',
 	enforce: 'pre',
 	load: (id: string): string | undefined => (matcher.test(id) ? '' : undefined),
