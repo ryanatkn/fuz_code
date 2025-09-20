ee1859706d884cd41ccee985d15212da15430827
Ryan Atkinson <mail@ryanatkn.com>
2025-09-20 15:56:53 -0400

    wip

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,8 @@
 	import '@ryanatkn/moss/theme.css';
 	// TODO BLOCK switch over after fixes
 	// import '$lib/theme_merged.css';
+	import '$lib/theme.css';
+	import '$lib/theme_highlight.css';
 	import '$routes/moss.css';
 
 	import Themed from '@ryanatkn/fuz/Themed.svelte';
