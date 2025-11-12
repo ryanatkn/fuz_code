305cc6b36a9d0154dcc3c821c39f8bd059cedd0e
Ryan Atkinson <mail@ryanatkn.com>
2025-11-12 10:04:10 -0500

    wip

--- src/lib/Code.svelte
+++ src/lib/Code.svelte
@@ -105,9 +105,25 @@
 
 	// TODO BLOCK `white-space: pre` customization how?
 	// TODO BLOCK keep padding override? only for non-inline?
+	// TODO BLOCK think through with the .pre class from Moss (maybe change it?)
+	// .pre {
+	// 	font-family: var(--font_family_mono);
+	// 	color: var(--text_color, var(--text_color_3));
+	// 	overflow: auto;
+	// 	max-width: 100%;
+	// }
+	// .pre > code {
+	// 	font-size: var(
+	// 		--font_size_sm
+	// 	); /* TODO @many use a var? maybe computed from generic `--font_size`? */
+	// 	font-weight: 500;
+	// }
+	// .pre:not(.inline) > code {
+	// 	display: block;
+	// }
 </script>
 
-<code {...rest} class:code={true} class:inline class:pre={inline} data-lang={lang}
+<code {...rest} class:inline class:pre={inline} data-lang={lang}
 	>{#if highlighting_disabled}{content}{:else if children}{@render children(
 			html_content,
 		)}{:else}{@html html_content}{/if}</code
